4861d2090aa75f05ea1977f15bd02aebc471919a
j271 <j271@pjwstk.edu.pl>
2025-08-13 10:35:02 +0200

    feat: create function to convert utc time to warsaw timezone time, install pytz package for timezone calculations

--- api/services/ztm.py
+++ api/services/ztm.py
@@ -1,9 +1,11 @@
 import httpx
-from typing import List, Dict
+from typing import List, Dict, Union
+from datetime import datetime
+import pytz
 # Brama Wyżynna
 ZTM_DISPLAYS_URL = "https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/ee910ad8-8ffa-4e24-8ef9-d5a335b07ccb/download/displays.json"
 ZTM_STOPS_URL = "https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4c4025f0-01bf-41f7-a39f-d156d201b82b/download/stops.json"
-
+WARSAW_TIMEZONE =  pytz.timezone("Europe/Warsaw")
 
 async def fetch_displays() -> List[Dict]:
     async with httpx.AsyncClient() as client:
@@ -24,3 +26,10 @@ async def fetch_stops() -> List[Dict]:
         data = response.json()
         print(f'fetch_stops data : {data}')
         return data.get("stops", [])
+
+def convert_utc_to_warsaw(utc_str: str) -> Union[str, None]:
+    if not utc_str:
+        return None
+    dt_utc = datetime.fromisoformat(utc_str.replace("Z", "+00:00"))
+    dt_warsaw = dt_utc.astimezone(WARSAW_TIMEZONE)
+    return dt_warsaw.strftime("%Y-%m-%d %H:%M:%S")
