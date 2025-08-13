00282627527b259845a9b46c784797c3457bf6ea
j271 <j271@pjwstk.edu.pl>
2025-08-13 10:11:50 +0200

    feat: implement CORSMiddleware and basic entry route

--- api/main.py
+++ api/main.py
@@ -1,7 +1,17 @@
+from fastapi import FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi import FastAPI
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/ping")
 def ping_api():
     return {"message": "API is working"}
+
